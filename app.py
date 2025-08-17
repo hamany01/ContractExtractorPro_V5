5341c41c46dfabbf2ab3cd58ae826c5a2cf45f49
hamany01 <hamany01@gmail.com>
2025-08-17 17:12:27 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "🎉 مرحبًا بك في JPF Contract Analyzer - الإصدار 5"
+
+if __name__ == "__main__":
+    app.run(debug=True)
